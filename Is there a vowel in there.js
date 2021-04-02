b09b0414404301ba831abfca43b469ef02121935
Thiago Ribeiro <46843472+ThigaoRibeiro@users.noreply.github.com>
2021-04-01 21:10:01 -0300

    Update Is there a vowel in there.js

--- Is there a vowel in there.js
+++ Is there a vowel in there.js
@@ -8,28 +8,119 @@ Test.assertSimilar(
      );
 */
 
+//let n1 = ["Z","X","D","E","R","V","A","J","T"]
 
+
+/*Exemplo de função
+==========================================
 function organizador(a,b){
     return (a-b)
   }
-
-let array = [118,117,120,121,117,98,122,97,120,106,104,116,113,114,113,120,106,118,"u",120,121,"u",98,122,"a",120,106,104,116,113,114,113,120,106,101,121,110,113,113,103,121,121,101,107,103,"e",121,110,113,113,103,121,121,"e",107,103]
-
-let n1 = ["Z","X","D","E","R","V","A","J","T"]
-let n2 = array.map(String.fromCharCode.apply(this, array))
+==========================================
+*/
 
 
 
-var unCharCode = function(x) {
-    return this["eval"]("String['fromCharCode'](" + x + ")");
-  };
-
-  unCharCode[118,117,120,121,117,98,122,97,120,106,104,116,113,114,113,120,106,118,"u",120,121,"u",98,122,"a",120,106,104,116,113,114,113,120,106,101,121,110,113,113,103,121,121,101,107,103,"e",121,110,113,113,103,121,121,"e",107,103]
-
-console.log(unCharCode)
-
+/*Exemplo de função
+==========================================
+let n2 = Array.prototype.filter.call (n1, char => "aeiou" .includes(n1)). join ("")
+return n2
+==========================================
+*/
 
 
-/*console.log(array.sort(organizador))
-console.log(array)*/
+
+/*Exemplo de função
+==========================================
+let vogais = /^[aeiou]/i;
+let matrix = n1.filter(str => vogais.test(str))
+==========================================
+*/
+
+
+
+/*Exemplo de função mais promiissor
+==========================================
+function isVow(a){
+let vowels = ["a","e","i","o","u","A","E","I","O","U"]
+let n1 = Array.from(String.fromCharCode.apply(this, a))
+let
+return n1
+}
+console.log(`${isVow(array)}`)
+==========================================
+*/
+
+
+/*Exemplo de função
+====================================================
+const vowelsAndConsonants = (str) => {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    // Check for vowels
+
+    var letters = str.split('');
+    var vowelsFound = [], consonantsFound = [];
+
+    for (var i in letters) {
+        if (vowels.includes(letters[i])) {
+            vowelsFound.push(letters[i]);
+        } else {
+            consonantsFound.push(letters[i]);
+        }
+    }
+    
+    console.log("Vowels:", vowelsFound.join(""));
+    console.log("Consonants:", consonantsFound.join(""));    
+}
+
+var str = "ThisIsATest";
+ vowelsAndConsonants(str.toLowerCase())
+ ====================================================
+ */
+
+/*Criação da função que vai codificar os numeros em letras
+=============================================================
+function isVow(n){
+let n2 = Array.from(String.fromCharCode.apply(this, n))
+map(function(item, index, array){
+return n2
+}
+=============================================================
+*/
+
+/*
+([118,117,120,121,117,98,122,97,120,106,104,116,113,114,113,120,106],[118,"u",120,121,"u",98,122,"a",120,106,104,116,113,114,113,120,106]);
+([101,121,110,113,113,103,121,121,101,107,103],["e",121,110,113,113,103,121,121,"e",107,103]);
+*/
+
+let array = [118,117,120,121,117,98,122,97,120,106,104,116,113,114,113,120,106]
+let vogais = ["a","e","i","o","u"]
+
+
+
+/**resolução do github*/
+const isVow = a =>
+  a.map(
+    charCode =>
+      /[aeiou]/.test(String.fromCharCode(charCode))
+        ? String.fromCharCode(charCode)
+        : charCode
+  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
